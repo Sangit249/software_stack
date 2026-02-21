974095b88bafa5ca2dd8ffa3e6c1c9086c580544
sangittkha-del <sangittkha@gmail.com>
2026-02-21 14:26:21 +0000

    Updated pug layout and app changes

--- app/app.js
+++ app/app.js
@@ -4,6 +4,10 @@ const express = require("express");
 // Create express app
 var app = express();
 
+// Use the Pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
 // Add static files location
 app.use(express.static("static"));
 
@@ -11,8 +15,10 @@ app.use(express.static("static"));
 const db = require('./services/db');
 
 // Create a route for root - /
+// Create a route for root
+// Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("Hello sangit"); 
+    res.render("index", {'title':'My index page', 'heading':'My heading'});
 });
 
 // Create a route for testing the db
@@ -23,8 +29,29 @@ app.get("/db_test", function(req, res) {
         console.log(results);
         res.send(results)
     });
-});
 
+});
+app.get("/programmes", function(req, res) {
+    var sql = 'select * from Programme';
+    var output ='table border="1px"';
+    db.query(sql).then(results => {
+        for(var row of results) {
+            output+='<tr>';
+            output+='<td>'+row.programme_id+'</td>';
+           output+="<td>" + "<a href='/programmes/"+row.programme_id+"'>"+row.programme_name+"</a>"+"</td>";
+            output+='</tr>';
+        }
+        output+='</table>';
+        res.send(output);
+    });
+});
+app.get("/allstudents", function(req, res){
+    sql ='select *from students';
+    db.query(sql).then(results => {
+        console.log(results);
+        res.json(results);
+    })
+});
 // Create a route for /goodbye
 // Responds to a 'GET' request
 app.get("/goodbye", function(req, res) {
@@ -34,7 +61,8 @@ app.get("/goodbye", function(req, res) {
 // Responds to a 'GET' request
 app.get("/roehampton", function(req, res) {
     console.log(req.url);
-    res.send("Welcome to  Roehampton University");
+    let path = req.url;
+    res.send(path.substring(0,4));
 });
 
 // Create a dynamic route for /hello/<name>, where name is any value provided by user
@@ -46,6 +74,51 @@ app.get("/hello/:name", function(req, res) {
     console.log(req.params);
     //  Retrieve the 'name' parameter and use it in a dynamically generated page
     res.send("Hello " + req.params.name);
+});
+// dynamic routing 
+app.get("/student/:name/:id",function(req, res){
+    let name = req.params.name;
+    let id = req.params.id;
+    // sending HTml as a string
+    res.send(`
+        <table border="1" >
+            <tr><th>Name </th><th>Id</th></tr>
+            <tr><td>${name}</td><td>${id}</td></tr>
+        </table>
+        `);
+
+});
+
+app.get("/db_test/:id", function(req,res) {
+    // capture the id from the url
+    const requestedId = req.params.id;
+    // prepare the SqL query
+    // we select 'name from  test_table where id matches
+    const sql = "SELECT name FROM test_table WHERE id = ?";
+    // execute using the db object from the scaffolding file
+    db.query(sql, [requestedId],(err,result) =>{
+        if (err){
+            console.error(err);
+            return res.status(500).send("Database error");
+
+        }
+        // logic did we find  arow
+        if(result.length>0) {
+            // result is  an array like[{name: lisa}]
+            const userName = result[0].name;
+            // send formated html responses
+            res.send(`
+                <div style="font-family:Arial; border:2px solid #333;padding:20px; border-radius: 10px; width:300px;">
+                    <h2 style="color: #007bf;">User found!</h2>
+                    <p><strong>ID:</strong> ${requestedId}</p>
+                    <p><strong>Name:</strong> ${userName}</p>
+                </div>
+            `);
+        }else{
+            // if the user enters an id tha doestnot exist 
+            res.send(`<h1> NO USer founf with Id: ${requestedId}</h1>`);
+        }
+    });
 });
 
 // Start server on port 3000
